06e9e7529b441b6d6e13d0f58a04132dc8e97e2b
fish <luk1684tw@gmail.com>
2017-06-16 03:30:48 +0800

    [Update]SideBar Icon set

--- src/components/DrawerSideBar.js
+++ src/components/DrawerSideBar.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {View, Text, Image, Platform} from 'react-native'
-
-import {Container, Content, Thumbnail, Icon, Badge, Button, Text as NbText, List, ListItem, Separator} from 'native-base';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import {Container, Content, Thumbnail, Badge, Button, Text as NbText, List, ListItem, Separator} from 'native-base';
 import appColors from '../styles/colors';
 
 export default class DrawerSideBar extends React.Component {
@@ -20,30 +20,37 @@ export default class DrawerSideBar extends React.Component {
                         <Text>待辦事項</Text>
                     </Separator>
                     <ListItem button onPress={() => navigate('Today')}>
+                        <Icon name='bomb' size={30}/>
                         <Text style={styles.text}>今天</Text>
                     </ListItem>
                     <ListItem button onPress={() => navigate('Upcoming')}>
+                        <Icon name='clock-alert' size={30}/>
                         <Text style={styles.text}>即將來臨</Text>
                     </ListItem>
                     <ListItem button onPress={() => navigate('SomeDay')}>
+                        <Icon name='timetable' size={30}/>
                         <Text style={styles.text}>選擇一天</Text>
                     </ListItem>
                     <Separator bordered>
                         <Text>群組</Text>
                     </Separator>
                     <ListItem button onPress={() => navigate('Group')}>
+                        <Icon name='tag-multiple' size={30}/>
                         <Text style={styles.text}>群組1</Text>
                     </ListItem>
                     <ListItem button onPress={() => navigate('Group')}>
+                        <Icon name='tag-multiple' size={30}/>
                         <Text style={styles.text}>群組2</Text>
                     </ListItem>
                     <ListItem button onPress={() => navigate('Group')}>
+                        <Icon name='tag-multiple' size={30}/>
                         <Text style={styles.text}>群組3</Text>
                     </ListItem>
                     <Separator bordered>
                         <Text>設定</Text>
                     </Separator>
                     <ListItem button onPress={() => navigate('Setting')}>
+                        <Icon name='settings-box' size={30}/>
                         <Text style={styles.text}>個人設定</Text>
                     </ListItem>
                 </List>
