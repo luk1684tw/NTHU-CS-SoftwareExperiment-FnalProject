4f779a5ee81ed7296c21b4d9579378ca2a3ff393
fish <luk1684tw@gmail.com>
2017-06-15 20:34:27 +0800

    [Update]backup

--- src/components/TodayScreen.js
+++ src/components/TodayScreen.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {View, Image} from 'react-native';
 import {Container, Icon, Fab, Button, Toast} from 'native-base';
-
+import NavigationContainer from './NavigationContainer.js';
 
 
 export default class TodayScreen extends React.Component {
@@ -10,5 +10,10 @@ export default class TodayScreen extends React.Component {
     constructor(props){
         super(props);
     }
-
+    render() {
+        const {navigate} = this.props.navigation;
+        return (
+            <NavigationContainer navigate={navigate}/>
+        )
+    }
 }
