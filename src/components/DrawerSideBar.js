094010cbd35941a6ed8b065db7d4d842c5836ac2
fish <luk1684tw@gmail.com>
2017-06-16 03:12:22 +0800

    [Update]SideBar UI beautified

--- src/components/DrawerSideBar.js
+++ src/components/DrawerSideBar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {View, Text, Image, Platform} from 'react-native'
 
-import {Container, Content, Thumbnail, Icon, Badge, Button, Text as NbText, List, ListItem} from 'native-base';
+import {Container, Content, Thumbnail, Icon, Badge, Button, Text as NbText, List, ListItem, Separator} from 'native-base';
 import appColors from '../styles/colors';
 
 export default class DrawerSideBar extends React.Component {
@@ -16,20 +16,35 @@ export default class DrawerSideBar extends React.Component {
         <Container style={styles.drawer}>
             <Content>
                 <List>
+                    <Separator bordered>
+                        <Text>待辦事項</Text>
+                    </Separator>
                     <ListItem button onPress={() => navigate('Today')}>
-                        <Text style={styles.text}>Today</Text>
+                        <Text style={styles.text}>今天</Text>
                     </ListItem>
                     <ListItem button onPress={() => navigate('Upcoming')}>
-                        <Text style={styles.text}>Upcoming</Text>
+                        <Text style={styles.text}>即將來臨</Text>
                     </ListItem>
                     <ListItem button onPress={() => navigate('SomeDay')}>
-                        <Text style={styles.text}>SomeDay</Text>
+                        <Text style={styles.text}>選擇一天</Text>
+                    </ListItem>
+                    <Separator bordered>
+                        <Text>群組</Text>
+                    </Separator>
+                    <ListItem button onPress={() => navigate('Group')}>
+                        <Text style={styles.text}>群組1</Text>
                     </ListItem>
                     <ListItem button onPress={() => navigate('Group')}>
-                        <Text style={styles.text}>Group</Text>
+                        <Text style={styles.text}>群組2</Text>
                     </ListItem>
+                    <ListItem button onPress={() => navigate('Group')}>
+                        <Text style={styles.text}>群組3</Text>
+                    </ListItem>
+                    <Separator bordered>
+                        <Text>設定</Text>
+                    </Separator>
                     <ListItem button onPress={() => navigate('Setting')}>
-                        <Text style={styles.text}>Settings</Text>
+                        <Text style={styles.text}>個人設定</Text>
                     </ListItem>
                 </List>
             </Content>
@@ -41,7 +56,7 @@ export default class DrawerSideBar extends React.Component {
 const styles = {
     drawer: {
         flex: 1,
-        backgroundColor: appColors.primaryLight
+        backgroundColor: 'white'
     },
     header: {
         width: undefined,
