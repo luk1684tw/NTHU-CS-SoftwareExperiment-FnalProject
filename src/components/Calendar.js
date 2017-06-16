57f7c81e75a3c7d6525473d01a7d55f2709e8743
chimaomao <a0961275586@gmail.com>
2017-06-16 20:32:36 +0800

    [update] add calendar

---
+++ src/components/Calendar.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import {Text,StyleSheet,ScrollView} from 'react-native';
+import {Calendar} from 'react-native-calendars';
+import NavigationContainer from './NavigationContainer';
+
+export default class SomeDayScreen extends React.Component{
+
+    constructor(props) {
+        super(props);
+          this.state = {};
+          this.onDayPress = this.onDayPress.bind(this);
+    }
+
+    render () {
+        const {navigate} = this.props.navigation;
+        return (
+        <NavigationContainer navigate={navigate} title='Calendar'>
+        <ScrollView style={styles.container}>
+        <Calendar
+          onDayPress={this.onDayPress}
+          style={styles.calendar}
+          hideExtraDays
+          markedDates={{[this.state.selected]: {selected: true}}}
+        />
+      </ScrollView>
+      </NavigationContainer>
+    );
+  };
+
+    onDayPress(day) {
+    this.setState({
+      selected: day.dateString
+    });
+  }
+}
+
+
+const styles = StyleSheet.create({
+  calendar: {
+    borderTopWidth: 1,
+    paddingTop: 5,
+    borderBottomWidth: 1,
+    borderColor: '#eee',
+    height: 350
+  },
+  text: {
+    textAlign: 'center',
+    borderColor: '#bbb',
+    padding: 10,
+    backgroundColor: '#eee'
+  },
+  container: {
+    flex: 1,
+    backgroundColor: 'gray'
+  }
+});
