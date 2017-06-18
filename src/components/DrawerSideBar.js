85eb7f701602053e0830594376a7a89b837c85ea
chimaomao <a0961275586@gmail.com>
2017-06-18 20:44:08 +0800

    [noupdate]

--- src/components/DrawerSideBar.js
+++ src/components/DrawerSideBar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {View, Text, Image, Platform} from 'react-native'
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import {Container, Content, Thumbnail, Badge, Button, Text as NbText, List, ListItem, Separator, Left, Body, Right} from 'native-base';
+import {Container, Content, Thumbnail, Badge, Button, Text as NbText, List, ListItem, Separator, Left, Body, Right ,Segment} from 'native-base';
 import appColors from '../styles/colors';
 import {setGroupScreenName} from '../states/event-actions';
 import {connect} from 'react-redux';
@@ -30,7 +30,7 @@ class DrawerSideBar extends React.Component {
         var items=['和學妹出去玩', '和妹妹野餐', '和女友約會'];
         let children=(
           <ListItem>
-              <Icon name='tag-multiple' size={30}/>
+              <Icon name='tag-multiple' size={24}/>
               <Text style={styles.text}>新增群組</Text>
           </ListItem>
         );
@@ -39,7 +39,7 @@ class DrawerSideBar extends React.Component {
               <List dataArray={items}
               renderRow={(item) =>
                   <ListItem button onPress={() =>  {this.handleOnClick(item)}}>
-                      <Icon name='tag-multiple' size={30}/>
+                      <Icon name='tag-multiple' size={20}/>
                       <Text style={styles.text}>{item}</Text>
                   </ListItem>
               }>
@@ -50,37 +50,39 @@ class DrawerSideBar extends React.Component {
 
       return (
         <Container style={styles.drawer}>
+
             <Content>
                 <List>
                     {/* 代辦事項 */}
-                    <ListItem itemDivider><Left><Text>待辦事項</Text></Left><Body></Body><Right></Right></ListItem>
+                    <ListItem itemDivider><Left><Text style={styles.text}>待辦事項</Text></Left><Body></Body><Right></Right></ListItem>
                     <ListItem button onPress={() => navigate('Today')}>
-                        <Icon name='bomb' size={30}/>
+                        <Icon name='bomb' size={20}/>
                         <Text style={styles.text}>今天</Text>
                     </ListItem>
-                    <ListItem button onPress={() => navigate('Upcoming')}>
-                        <Icon name='clock-alert' size={30}/>
+                    <ListItem  button onPress={() => navigate('Upcoming')}>
+                        <Icon name='clock-alert' size={20}/>
                         <Text style={styles.text}>即將來臨</Text>
                     </ListItem>
-                    <ListItem button onPress={() => navigate('SomeDay')}>
-                        <Icon name='timetable' size={30}/>
+                    <ListItem  button onPress={() => navigate('SomeDay')}>
+                        <Icon name='timetable' size={20}/>
                         <Text style={styles.text}>選擇一天</Text>
                     </ListItem>
                     {/* 群組 */}
                     <ListItem itemDivider>
-                        <Left><Text>群組</Text></Left>
+                        <Left><Text style={styles.text}>群組</Text></Left>
                         <Body></Body>
-                        <Right><Icon button name='tag-plus' size={24} onPress={() => navigate('CreateGroup')}/></Right>
+                        <Right><Icon button name='tag-plus' size={20} onPress={() => navigate('CreateGroup')}/></Right>
                     </ListItem>
                     {children}
                     {/* 設定 */}
-                    <ListItem itemDivider><Left><Text>設定</Text></Left><Body></Body><Right></Right></ListItem>
-                    <ListItem button onPress={() => navigate('Setting')}>
-                        <Icon name='settings-box' size={30}/>
+                    <ListItem itemDivider><Left><Text style={styles.text}>設定</Text></Left><Body></Body><Right></Right></ListItem>
+                    <ListItem  button onPress={() => navigate('Setting')}>
+                        <Icon name='settings-box' size={20}/>
                         <Text style={styles.text}>個人設定</Text>
                     </ListItem>
                 </List>
             </Content>
+
         </Container>
     );
     }
@@ -91,7 +93,7 @@ class DrawerSideBar extends React.Component {
 
 const styles = {
     drawer: {
-        flex: 1,
+        flex: 2,
         backgroundColor: 'white'
     },
     header: {
@@ -110,14 +112,18 @@ const styles = {
     },
     text: {
         color: appColors.primaryLightText,
-        fontSize: (Platform.OS === 'ios') ? 17 : 19,
+        fontSize: (Platform.OS === 'ios') ? 10 : 12,
         fontWeight: 'bold',
         flex: 1,
         marginHorizontal: 12
     },
     title:{
         backgroundColor: 'rgb(255, 219, 251)',
-        fontSize: (Platform.OS === 'ios')?17:19
+        fontSize: (Platform.OS === 'ios')? 10: 12
+    },
+    itemheight:{
+
+      flex:1
     }
 };
 export default connect((state, ownProps) => ({
