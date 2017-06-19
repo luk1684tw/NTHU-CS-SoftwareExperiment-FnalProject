0033c5b514d3a77831547c8cae707607fa53dbb0
weichu <ms0701515@gmail.com>
2017-06-20 03:28:22 +0800

    [chu]Redux done Event

--- src/components/CheckBox.js
+++ src/components/CheckBox.js
@@ -1,24 +1,48 @@
 import React from 'react';
 import {CheckBox} from 'react-native-elements';
 import {Container , Content} from 'native-base';
-
-export default class checkBox extends React.Component {
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import {finishEvent} from '../states/event-actions'
+class checkBox extends React.Component {
+    static propTypes={
+        id: PropTypes.string,
+        isDone: PropTypes.bool,
+        dispatch: PropTypes.func
+    }
     constructor(props) {
         super (props);
         this.state = {
             checked: false
         };
+        this.handleComplete=this.handleComplete.bind(this);
+    }
+    handleComplete(){
+        if(this.props.isDone===false)
+            this.props.dispatch(finishEvent(this.props.id));
     }
     render () {
         return (
             <CheckBox
-                containerStyle={{backgroundColor:'transparent',borderWidth:0,position:'absolute',right:10}}
+                containerStyle={styles.checkbox}
                 center
                 checkedColor='red'
                 uncheckedColor='black'
-                checked={this.state.checked}
-                onPress={()=> this.setState({checked: !this.state.checked})}
+                checked={this.prop.isDone}
+                onPress={this.handleComplete}
             />
         );
     }
 }
+const styles={
+    checkbox:{
+        backgroundColor:'transparent',
+        border:0,
+        position:'absolute',
+        // marginTop: -10
+        margin: 0,
+        borderWidth :0
+    }
+}
+export default connect((state, ownProps) => ({
+}))(CheckBox);
